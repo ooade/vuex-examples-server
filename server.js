7dd876e4c8a86849f4d58ab38914924534b232da
Ademola Adegbuyi <ooade96@gmail.com>
2017-09-28 11:43:49 +0100

    Dynamic port

--- server.js
+++ server.js
@@ -62,4 +62,6 @@ app.post('/api/delete/images', (req, res) => {
 	})
 })
 
-app.listen(3000, () => console.log('Server Listening on PORT 3000'))
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => console.log(`Server Listening on PORT ${PORT}`))
